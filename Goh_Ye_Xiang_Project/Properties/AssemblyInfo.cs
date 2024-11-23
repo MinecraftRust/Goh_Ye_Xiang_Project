e27059f85e58156b54dc6a5fa645e5d7164dfcc6
Ye Xiang <goh_ye_xiang@connect.ite.edu.sg>
2024-11-23 12:32:01 +0800

    ~Labsheet 4 Changes

--- Goh_Ye_Xiang_Project/Properties/AssemblyInfo.cs
+++ Goh_Ye_Xiang_Project/Properties/AssemblyInfo.cs
@@ -12,9 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Goh_Ye_Xiang_Project")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5154585d6f4c3e9eba3b191171614e0c44053ed7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d4c9588704151b8bdda0a6182935527ea6f3605")]
 [assembly: System.Reflection.AssemblyProductAttribute("Goh_Ye_Xiang_Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Goh_Ye_Xiang_Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
